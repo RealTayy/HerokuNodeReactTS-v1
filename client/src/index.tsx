b0c26500ea583c5a6e52cea009297e10936943f1
The Mai <mai.the.09@gmail.com>
2020-05-07 14:00:09 -0500

    Initial files and folders

---
+++ client/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import { configure } from "mobx";
+
+// MobX Configuration
+configure({
+  computedRequiresReaction: true,
+  observableRequiresReaction: true,
+  reactionRequiresObservable: true,
+  enforceActions: "observed"
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
